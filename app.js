21648ff734187b08319241da75ba54c1122b52ea
berrouyne <berry.othmane@gmail.com>
2025-12-24 03:06:53 +0100

    feat: real UI with CSS styling

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-document.getElementById("msg").innerText =
-  "Deployed automatically using GitHub Actions + ArgoCD";
+const statusEl = document.getElementById("status");
+const updatedAtEl = document.getElementById("updatedAt");
+
+statusEl.innerText = "✅ Application is running";
+updatedAtEl.innerText = new Date().toLocaleString();
